e307a5d96a6de0203a59652bd82936a5e7b41b9b
kendos31 <rikesim@gmail.com>
2024-09-16 16:52:35 +0300

    First Commit

---
+++ src/main/java/com/kendos/fleetms/ApplicationController.java
@@ -0,0 +1,13 @@
+package com.kendos.fleetms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+	@GetMapping("/index")
+	public String home() {
+		return "index";
+	}
+
+}
